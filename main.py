269265a7be60f71efa34e680b19acc4ec2b89f68
Fariz Apriyanto <fapriyanto@alterra.id>
2024-01-16 09:13:31 +0700

    test

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import uvicorn
+import multiprocessing
 # from  import Process, freeze_support
 
 
 if __name__ == "__main__":
+    mutiprocessing.freeze_support()
     uvicorn.run("app:app", host="0.0.0.0", port=8080, reload=False)
